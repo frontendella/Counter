62eff96bf0d7a442c89c3ae191a912997cd1c325
Ella R <ryukhinaella@gmail.com>
2022-08-02 13:27:36 -0700

    Change state with a callback function

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   function handleIncrement() {
-    setCount(count + 1);
+    setCount((prevCount) => prevCount + 1);
   }
 
   function handleDecrement() {
-    setCount(count - 1);
+    setCount((prevCount) => prevCount - 1);
   }
 
   return (
